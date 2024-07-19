595ff09f5d641800d3e7f88db301bae10b45a3e4
ale18V <alebombarda@icloud.com>
2024-07-19 20:14:23 +0200

    Resolve merge conflicts

--- Beginners/Web3/tailwind.config.js
+++ Beginners/Web3/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: ["src/public/**/*.html"],
-=======
   content: ["src/public/**/*.html", 'views/**/*.{html,ejs}'],
->>>>>>> ale/dev
   theme: {
     extend: {},
   },
