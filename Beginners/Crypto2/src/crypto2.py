46dab42e4f0e3c4aea44614b495b9a95e4161880
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-09-25 15:04:46 +0200

    finished crypto2 challenge and fixed some

--- Beginners/Crypto2/src/crypto2.py
+++ Beginners/Crypto2/src/crypto2.py
@@ -1,28 +1,26 @@
-import os, dotenv
+import os
 
-dotenv.load_dotenv()
 SENTENCES = [
     b"Elia recently passed away, how will we be able to live without a sysadmin?!!?",
     os.urandom(42),
     os.getenv('FLAG', 'pascalCTF{m4by3_1_sh0uld_ch3ck_th3_t0t4l_numb3r_0f_ONES}').encode()
 ]
-assert int.from_bytes(SENTENCES[2], 'big').bit_length() == 447
 
-def play(data: list[int], idx: int) -> list[int]:
-    result = [0]
+def play(bets: list[int], idx: int) -> list[int]:
+    result = []
     if idx < 0 or idx > 2:
         return result
     encoded = int.from_bytes(SENTENCES[idx], 'big')
-    for mask in data:
+    for bet in bets:
         # why you're using 1s when 0s exist
-        if mask.bit_count() > 40:
+        if bet.bit_count() > 40:
             continue
-        result.append(encoded & mask)
+        result.append(encoded & bet)
     
     return result
 
 def menu():
-    print("Welcome to the mindBlowing game!")
+    print("Welcome to the italian MindBlowing game!")
     print("1. Play the game")
     print("2. Exit")
     print()
@@ -36,11 +34,11 @@ if __name__ == '__main__':
         try:
             if choice == '1':
                 idx = int(input('Gimme a number in the range [0; 2]: '))
-                datas = int(input('Gimme the number of your bets: '))
-                data = []
-                for _ in range(datas):
-                    data.append(int(input(f'Gimme the bet {_+1}: ')))
-                print(f"Result: {play(data, idx)}")
+                bets_num = int(input('Gimme the number of your bets: '))
+                bets = []
+                for _ in range(bets_num):
+                    bets.append(int(input(f'Gimme the bet {_+1}: ')))
+                print(f"Result: {play(bets, idx)}")
             elif choice == '2':
                 break
             else:
